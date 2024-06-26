2ff5ee864c8857ac270edb37d17766bda29e5b53
salembht <salembahattab22@gmail.com>
2024-06-26 15:11:12 +0300

    complate the api for the account

--- server/app/Http/Controllers/AccountController.php
+++ server/app/Http/Controllers/AccountController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AccountController extends Controller
 {
-    
+    public function login(Request $request)
+    {
+     if(!Auth::attempt([
+            'email'=>$request->email,
+            'password'=>$request->password 
+        ]))
+        {
+            return response(['message'=>'الايميل او كلمه السر خاطئة'],Response::HTTP_UNAUTHORIZED);
+        }
+        $user=Auth::user();
+        $token=$user->createToken('token')->plainTextToken;
+        $cooki =cookie('jwt',$token,60*24*30);
+        return response([
+            'message'=>"تم تسجيل الدخول بنجاح"
+        ])->withCookie($cooki);
+    }   
+    public function register(Request $request)
+    {
+        return $user=User::create([
+            'name'=>$request->username,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password) ,
+        ]);
+        
+    }  
+    public function user()
+    {
+        return Auth::user();
+    }
+    public function logout()
+    {
+        $cookie=Cookie::forget('jwt');
+        return response([
+            'message'=>"تم تسجل خروجك بنجاح"
+        ])->withCookie($cookie);
+    }
 }
--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('register',[AccountController::class,'register']);
+Route::post('login',[AccountController::class,'login']);
+Route::post('logout',[AccountController::class,'logout']);
+Route::middleware('auth:sanctum')->group(function(){
+Route::get('user',[AccountController::class,'user']);
+});
